a3833f283f03c49beffba68b1ef53c0e3ce5299d
Sakchai.P <sakchai.pa@arise.tech>
2025-02-07 00:04:22 +0700

    :tada: feat: navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,5 @@
+<section class="fixed left-0 top-0 flex w-full items-center justify-between px-8">
+	<h1>portfolio</h1>
+
+	<p class="mt-6 font-mono">BANGKOK, THAILAND <br />&ANYWHERE</p>
+</section>
