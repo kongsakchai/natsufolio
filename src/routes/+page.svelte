dcdce6a273c72cfb7f86e07852ba973775811a78
kongsakchai <ksakchai.dev@gmail.com>
2025-02-09 15:51:05 +0700

    :tada: feat: add contact page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <h1 class="tracking-wider">Natthamon Pienrakkarn, <br /> Natsu</h1>
+<p class=" font-mono text-sm">Graphic Designer</p>
 
 <section class="mx-auto mt-10 max-w-[414px]">
 	<Emoji />
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,8 @@
+<h1>CONTACT</h1>
+
+<section class="mt-10 max-w-[414px] text-nowrap pl-36 font-mono">
+	<article>
+		<h5 class="font-normal">/ E-mail</h5>
+		<p class="text-xs">natsudsn.work@gmail.com</p>
+	</article>
+</section>
