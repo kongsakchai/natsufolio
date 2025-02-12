38039a1071a9ad5a1feb0a7994eb698d1ea0a880
Kong Sakchai <55359787+kongsakchai@users.noreply.github.com>
2025-02-12 18:27:27 +0700

    :fire: fix: video path

--- src/lib/assets/index.ts
+++ src/lib/assets/index.ts
@@ -1,3 +1,3 @@
 export { default as CardBack } from './card-back.png';
 export { default as CardFront } from './card-front.png';
-export { default as Emoji } from './emoji.mp4';
+export { default as Emoji } from './Emoji.mp4';
