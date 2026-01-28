af7e8b64f2ea328e1206f48cc884a1da889b0d13
kongsakchai <ksakchai.dev@gmail.com>
2026-01-28 08:47:05 +0700

    :wrench: fix home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <PageTransition>
 	<h1 class="header tracking-wider">Natthamon Pienrakkarn, <br /> Natsu</h1>
 	<!-- <h4 class="font-almarai font-normal">Graphic Designer</h4> -->
-
+	<!-- 
 	<section class="mx-2 mt-25 max-w-110 grid grid-cols-2 items-center">
 		<div class="flex flex-col gap-6 h-full justify-center">
 			<a href="/about"><h1 class="font-almarai font-normal">ABOUT ME</h1></a>
@@ -17,5 +17,8 @@
 		<div class="relative -top-10">
 			<Emoji />
 		</div>
+	</section> -->
+	<section class="mx-auto my-1 max-w-110 h-full">
+		<Emoji />
 	</section>
 </PageTransition>
