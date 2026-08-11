bb4e0bb17c086d5080f3145c1959bf7c54b771f7
Sakchai Paoin <55359787+kongsakchai@users.noreply.github.com>
2026-08-12 03:36:06 +0700

    Update +page.ts

--- src/routes/work/[tag]/+page.ts
+++ src/routes/work/[tag]/+page.ts
@@ -1,5 +1,5 @@
 import { projects } from '$lib/data';
 
 export const load = async ({ params }) => {
-	return { title: params.tag, projects: projects(params.tag) };
+	return { title: params.tag === "UXUI" ? "UX/UI":params.tag, projects: projects(params.tag) };
 };
